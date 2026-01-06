54bf826c8ab49e8957f703dcaa72d591826e276d
niteshmandall <niteshnunfara0@gmail.com>
2026-01-06 15:49:25 +0530

    Update create_tangerine_opds_v2.py

--- opds/tangerine/create_tangerine_opds_v2.py
+++ opds/tangerine/create_tangerine_opds_v2.py
@@ -8,7 +8,7 @@ from urllib.parse import quote
 BASE_URL = "https://ibiza-stage-tangerine-dev.web.app" 
 DATA_SOURCE_BASE = "https://tangerinestaging.ustadmobile.com"
 GROUP_LIST_URL = f"{DATA_SOURCE_BASE}/nest/group/list"
-AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwicGVybWlzc2lvbnMiOnsiZ3JvdXBQZXJtaXNzaW9ucyI6W10sInNpdGV3aWRlUGVybWlzc2lvbnMiOlsiY2FuX2NyZWF0ZV9ncm91cCIsImNhbl92aWV3X3VzZXJzX2xpc3QiLCJjYW5fY3JlYXRlX3VzZXJzIiwiY2FuX2VkaXRfdXNlcnMiLCJjYW5fbWFuYWdlX3VzZXJzX3NpdGVfd2lkZV9wZXJtaXNzaW9ucyJdfSwiaWF0IjoxNzY3NjA4OTgyLCJleHAiOjE3Njc2MTI1ODIsImlzcyI6IlRhbmdlcmluZSIsInN1YiI6InVzZXIxIn0.UxSjksmSkFgqwxGL8jGkQa3r6SJGRV_JCMdc9iUaOds"
+AUTH_TOKEN = "auth_token"
 
 OUTPUT_DIR = "public"
 GROUPS_DIR = os.path.join(OUTPUT_DIR, "groups")
